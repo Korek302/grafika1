63e10225773d96329e8f71eb561639622744612c
Korek302 <jacek_blady@interia.pl>
2017-10-15 01:25:10 +0200

    many circles

--- src/grafika1/ManyCircles.java
+++ src/grafika1/ManyCircles.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.awt.image.*;
 import javax.imageio.*;
 
-public class Example1 
+public class ManyCircles
 {
 	public static void main(String[] args)
 	{
@@ -15,6 +15,9 @@ public class Example1
 		int x_res;
 		int y_res;
 		
+		int x_squares;
+		int y_squares;
+		
 		int x_c;
 		int y_c;
 		
@@ -24,10 +27,15 @@ public class Example1
 		int i;
 		int j;
 		
-		final int w = 10;
+		final int squareW = 50;
+		
+		final int w = 5;
 		
 		x_res = 400;
 		y_res = 400;
+		
+		x_squares = x_res/squareW;
+		y_squares = y_res/squareW;
 		
 		image = new BufferedImage(x_res, y_res, BufferedImage.TYPE_INT_RGB);
 		
@@ -37,24 +45,31 @@ public class Example1
 		x_c = x_res/2;
 		y_c = y_res/2;
 		
-		for(i = 0; i < y_res; i++)
-			for(j = 0; j < x_res; j++)
-			{
-				double d;
-				int r;
-				
-				d = Math.sqrt((i-y_c)*(i-y_c) + (j-x_c)*(j-x_c));
-				
-				r = (int) d / w;
-				
-				if(r % 2 == 0)
-					image.setRGB(j, i, black);
-				else
-					image.setRGB(j, i, white);
-			}
+		
+		for(int m = 0; m < y_squares; m++)
+			for(int n = 0; n < x_squares; n++)
+				for(i = 0; i < squareW; i++)
+					for(j = 0; j < squareW; j++)
+					{
+						double d;
+						int r;
+						
+						x_c = n*squareW + squareW/2;
+						y_c = m*squareW + squareW/2;
+						
+						d = Math.sqrt(((squareW*m + i)-y_c)*((squareW*m + i)-y_c) 
+								+ ((squareW*n + j)-x_c)*((squareW*n + j)-x_c));
+						
+						r = (int) d / w;
+						
+						if(r % 2 == 0)
+							image.setRGB(squareW*n + j, squareW*m + i, black);
+						else
+							image.setRGB(squareW*n + j, squareW*m + i, white);
+					}
 		try
 		{
-			ImageIO.write(image, "bmp", new File("exp.bmp"));
+			ImageIO.write(image, "bmp", new File("manyCircles.bmp"));
 			System.out.println("Ring image created successfully");
 		}
 		catch(IOException e)
